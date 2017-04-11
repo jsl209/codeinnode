d68df3cf7fda706c86c85a873623b02f415747b6
info@blink.cloud <info@blink.cloud>
2017-04-11 15:08:55 -0400

    initializing repo

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.port; 
+
+
+app.get('/:param/:param2', function (req, res) {
+  var p1 = req.params.param;
+  var p2 = req.params.param2;
+  
+  result = p1+p2; 
+  res.status(200).json({
+    result: result
+  });
+});
+
+app.listen(port || 3000);
