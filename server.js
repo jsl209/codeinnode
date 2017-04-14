8fd360e18db2d1fba54293d030f48dbf8eefb666
info@blink.cloud <info@blink.cloud>
2017-04-14 16:21:13 -0400

    i modifed server.js and added route for '/'

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.port; 
 
+app.get('/', function (req, res) {
+  res.status(200).json({
+    status: 'nodemon updates this in realtime'
+  });
+});
 
 app.get('/:param/:param2', function (req, res) {
   var p1 = req.params.param;
